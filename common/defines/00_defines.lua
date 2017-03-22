0aa1220b7e14eff209a63906ba0eaa4e87c0fa28
Richard Bong <trmilescontact@gmail.com>
2017-03-21 23:19:57 -0500

    Events and Localisation

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -1,7 +1,7 @@
 NDefines = {
 
 NGame = {
-	START_DATE = "305.1.1.12",
+	START_DATE = "306.1.1.12",
 	END_DATE = "480.1.1.1",
 	MAP_SCALE_PIXEL_TO_KM = 7.114,					-- Yes, we did the math
 	SAVE_VERSION = 3,
